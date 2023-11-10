6e3fb316c77fae460e697287e08f4cf2634a353d
Maximiliano Usich <maximilianou@gmail.com>
2023-11-10 15:20:31 +0100

    feat: doc test

---
+++ devrust/adder/target/debug/deps/adder-432a4b12f342c896.d
@@ -0,0 +1,7 @@
+/home/kali/projects/weekly66/devrust/adder/target/debug/deps/adder-432a4b12f342c896.rmeta: src/lib.rs
+
+/home/kali/projects/weekly66/devrust/adder/target/debug/deps/libadder-432a4b12f342c896.rlib: src/lib.rs
+
+/home/kali/projects/weekly66/devrust/adder/target/debug/deps/adder-432a4b12f342c896.d: src/lib.rs
+
+src/lib.rs:
---
+++ devrust/adder/target/debug/deps/adder-f1a5664f84d74fb5.d
@@ -0,0 +1,5 @@
+/home/kali/projects/weekly66/devrust/adder/target/debug/deps/adder-f1a5664f84d74fb5: src/lib.rs
+
+/home/kali/projects/weekly66/devrust/adder/target/debug/deps/adder-f1a5664f84d74fb5.d: src/lib.rs
+
+src/lib.rs:
